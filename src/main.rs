3fb6f0a029a3aa8837074b6f472fc8a3090242c2
Desley Langeveld <desley.langeveld@hotmail.com>
2025-11-13 22:01:12 +0100

    The perma function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
-use crossterm::{terminal, ExecutableCommand};
+use clap::{Parser, Subcommand};
+// use crossterm::{ExecutableCommand, terminal};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Perma {
+        site: String
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Commands::Perma {site} => {
+            perma(site)
+        }
+    }
+}
+
+fn perma(site: String)-> Result<(), std::io::Error>{
+    let url = format!("https://{}", site);
+    open::that(url)?;
+    Ok(())
 }
