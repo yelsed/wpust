2dc3ca1281d6fd8b8aa041531696f7423404f94f
Desley Langeveld <desley.langeveld@hotmail.com>
2025-11-14 21:53:59 +0100

    Working permalink function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use color_eyre::eyre::Result;
 // use crossterm::{ExecutableCommand, terminal};
 
 #[derive(Parser, Debug)]
@@ -15,18 +16,43 @@ enum Commands {
     }
 }
 
-fn main() {
+fn main() -> Result<()> {
+    color_eyre::install()?; 
     let args = Args::parse();
 
     match args.command {
         Commands::Perma {site} => {
-            perma(site)
+            let site_after_check = acccheck(site);
+            perma(site_after_check)?;
         }
     }
+    
+    Ok(())
 }
 
-fn perma(site: String)-> Result<(), std::io::Error>{
-    let url = format!("https://{}", site);
+fn acccheck(site: String) -> String { 
+    let acceptation_basic_auth = "https://REDACTED:REDACTED@";
+    let acceptation_string = "acc.";
+
+    let url = if site.contains(acceptation_string) {
+        acceptation_basic_auth.to_owned() + &site.to_string()
+    } else {
+        format!("https://{}", site)
+    };
+
+    url
+}
+
+fn perma(site: String) -> Result<()> {
+    if site.is_empty() {
+        return Err(color_eyre::eyre::eyre!("Site name may not be empty"));
+    }
+   
+    let url = site + "/wp-admin/options-permalink.php";
+
+    println!("{}", url);
+
     open::that(url)?;
     Ok(())
 }
+
